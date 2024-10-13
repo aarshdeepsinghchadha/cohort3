e5725e518a8356710121afc68bef2482dce104f6
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-10-13 17:29:09 +0530

    added a part of code which lets you add the post and rerender the dom

--- Week 9 Offline/week-9-offline/src/App.jsx
+++ Week 9 Offline/week-9-offline/src/App.jsx
@@ -1,33 +1,44 @@
 import { PostComponent } from "./Post";
 import { GreetingComponent } from "./Greeting";
+import { useState } from "react";
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  const postComponents = posts.map(post => <PostComponent
+    imageSrc={post.imageSrc}
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    description={post.description}
+  />)
+
+  function addPost() {
+    setPosts([
+      ...posts,
+      {
+        imageSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png",
+        name: "Lego",
+        subtitle: "23,777 followers",
+        time: "12 mins",
+        description: "Let's play with toys? Which one is your favorite?",
+      },
+    ]);
+  }
+
   return (
     <div style={{ backgroundColor: "#ecf0f1", height: "100vh", position: "relative" }}>
       {/* Greeting Component that spans horizontally */}
       <GreetingComponent greeting="Hello Aarsh" />
-
+      <div style={{ paddingLeft: 20 }}>
+        <button onClick={addPost}>Add Post</button>
+      </div>
       {/* Posts start below the Greeting Component */}
       <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
         <div>
           <div>
-            <PostComponent
-              imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"
-              name="Lego"
-              subtitle="23,777 followers"
-              time="12 mins"
-              description="let's play with toys? which one is your fav?"
-            />
-            <br />
-          </div>
-          <div>
-            <PostComponent
-              imageSrc="https://cdn.logoworks.com/wp-content/uploads/2017/06/Untitled-2-640x360-1.png"
-              name="Starbucks"
-              subtitle="promoted"
-              description="Come have a coffee with us?"
-            />
-            <br />
+            {postComponents}
           </div>
         </div>
       </div>
