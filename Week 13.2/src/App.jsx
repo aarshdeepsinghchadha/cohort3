a152b21dc9ab067ab0c12b69b177ae01bd1f4f52
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-11-20 07:41:09 +0530

    completed the sidebar

---
+++ Week 13.2/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { SidebarClass1 } from './answers/1-basic.projects';
+function App() {
+  return (
+    <>
+      <SidebarClass1 />
+    </>
+  )
+}
+
+export default App
