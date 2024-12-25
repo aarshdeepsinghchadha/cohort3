beb706d6f05fa844a8992aadf0ec8a3073e650bd
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-12-25 19:42:13 +0530

    routes

---
+++ week-20/app/(auth)/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="w-screen h-16 bg-gray-800 flex items-center justify-center text-white">
+            This is Footer
+        </footer>
+    )
+}
--- week-20/app/(bounty)/chat/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChatPage() {
-    return (
-        <div className="w-screen h-screen flex items-center justify-center">
-            Hi this is the chat page
-        </div>
-    )
-}
