be13633c5148e0a3819dcaf912e2aa7564f9d3b4
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-12-25 19:18:46 +0530

    routing in next

---
+++ week-20/app/(bounty)/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function ChatPage() {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            Hi this is the chat page
+        </div>
+    )
+}
